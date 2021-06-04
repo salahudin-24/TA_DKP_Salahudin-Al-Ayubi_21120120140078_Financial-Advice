6ab5470e7dbf63b466d6b358f4234d92443125c9
salahudin-24 <56055107+salahudin-24@users.noreply.github.com>
2021-06-04 19:44:02 +0700

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,11 @@
+from tkinter import *
+from tkinter.tix import *
+
+def gridding(widget,row,column,rowspan=1,columnspan=1,sticky=W):
+    widget.grid(row=row, column=column, rowspan=rowspan,columnspan=columnspan, sticky=sticky)
+
+def getTOtalRatio(gui_data) :
+    RATIOtotal=0
+    for RATIOcategory in gui_data:
+        RATIOtotal+=RATIOcategory[0].get()
+    return RATIOtotal
